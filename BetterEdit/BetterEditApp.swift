4b7e8bb9b64e69c064f1fbf1a26cc0e1b3b3798e
Alireza Kamali <alireza.kamali@icloud.com>
2020-08-20 04:50:21 +0300

    Initial Commit

---
+++ BetterEdit/BetterEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterEditApp.swift
+//  BetterEdit
+//
+//  Created by Alireza Kamali on 2020-08-20.
+//
+
+import SwiftUI
+
+@main
+struct BetterEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
